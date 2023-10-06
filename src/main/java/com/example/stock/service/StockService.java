9a2d59885fa5648dcb87f10064f30d380206cb38
수지니 <sssssjin99@naver.com>
2023-10-06 12:38:10 +0900

    [feat]: 재고감소 로직작성

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,28 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    /**
+     * 재고 감소 메서드
+     */
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
